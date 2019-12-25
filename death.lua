1571cef1dc9d1895ef06b7d63c7aed9724e04e88
Disaster <51462873+Biven231@users.noreply.github.com>
2019-12-25 20:25:01 +0300

    bye

--- death.lua
+++
@@ -1,6 +0,0 @@
-local function HappyDeathSounds( ent )
-	ent:EmitSound( "garrysmod/save_load" .. math.random( 10 ) .. ".wav" )
-end
-
-hook.Add( "OnNPCKilled", "NPCHappyDeathSounds", HappyDeathSounds )
-hook.Add( "PlayerDeath", "PlayerHappyDeathSounds", HappyDeathSounds )
