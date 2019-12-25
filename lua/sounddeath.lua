512ffb38d424f60488d94420b6b2219b2920dd32
Disaster <51462873+Explorered@users.noreply.github.com>
2019-12-25 20:57:40 +0300

    Update sounddeath.lua

--- lua/sounddeath.lua
+++ lua/sounddeath.lua
@@ -1,1 +1,10 @@
+local function killplayer( ent )
+	ent:EmitSound( "garrysmod/save_load" .. math.random( 10 ) .. ".wav" )
+	
+	
+	
+end
+
+hook.Add( "OnNPCKilled", "NPCkillplayer", killplayer )
+hook.Add( "PlayerDeath", "Playerkillplayer", killplayer )
 
